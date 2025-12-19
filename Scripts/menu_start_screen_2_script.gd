31e962bc426e6101e719565762b0597841b66c95
Atticus <callum01eales@gmail.com>
2025-12-19 17:30:07 +0000

    movement 3

--- Scripts/menu_start_screen_2_script.gd
+++ Scripts/menu_start_screen_2_script.gd
@@ -26,14 +26,15 @@ func _process(delta: float) -> void:
 >
 >
 >
-> Use 'W' to move Forward. Use 'S' to move Backwards.
+> Use 'W' to move Forward. Use 'S' to preform a quick turn.
 > Use 'A' to rotate Left. Use 'D' to rotate Right.
 > Use 'E' to interact with objects that pulse blue.
 >
+> Objects that glow green will progress the subject's memory.
+> Please ignore any hallucinatory overalls that may be present.
 >
 > Press ENTER to continue...
->
-"
+>"
 	if Globals.InputType == 2:
 		$StartUpText.text = "
 > 
@@ -51,9 +52,10 @@ func _process(delta: float) -> void:
 > The Subject will always face and move towards the mouse's position
 > Use 'E' to interact with objects that pulse blue.
 >
+> Objects that glow green will progress the subject's memory.
+> Please ignore any hallucinatory overalls that may be present.
 >
 > Press ENTER to continue...
->
 >"
 	if Globals.InputType == 3:
 		$StartUpText.text = "
@@ -72,9 +74,10 @@ func _process(delta: float) -> void:
 > Use 'A' to move Left. Use 'D' to move Right.
 > Use 'E' to interact with objects that pulse blue.
 >
-> 
-> Press ENTER to continue...
+> Objects that glow green will progress the subject's memory.
+> Please ignore any hallucinatory overalls that may be present.
 >
+> Press ENTER to continue...
 >"
 
 func _on_change_menu_sfx_tree_exited() -> void:
