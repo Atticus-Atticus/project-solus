2530f668ac53119b4a4db06024feb6bc25a0137f
Atticus <callum01eales@gmail.com>
2026-02-03 23:26:36 +0000

    got new stuff coming

---
+++ Scripts/tutorial_level_script.gd
@@ -0,0 +1,28 @@
+extends Node3D
+
+@onready var pause_menu = $PauseMenu
+#@onready var text: PackedScene = preload("res://Scenes/User Interface/Dialogue/Loop0DialogueBox.tscn")
+@onready var cursor = $ProjectSolusCursor2
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#var LoadedText = text.instantiate()
+	#add_child(LoadedText)
+	pass
+
+
+func _process(delta: float) -> void:
+
+	if Input.is_action_just_pressed("Pause"):
+		pausemenu()
+
+
+func pausemenu():
+	if Globals.paused:
+		pause_menu.hide()
+		Engine.time_scale = 1
+	else:
+		pause_menu.show()
+		Engine.time_scale = 0
+	Globals.paused = !Globals.paused
