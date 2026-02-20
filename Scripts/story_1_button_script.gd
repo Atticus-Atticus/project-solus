ff2ab20cc1fa07a91dacc9cb3d8f108e09d31e02
Atticus <callum01eales@gmail.com>
2026-02-20 21:25:18 +0000

    pushing a small update to fix a bug

--- Scripts/story_1_button_script.gd
+++ Scripts/story_1_button_script.gd
@@ -7,4 +7,4 @@ func _on_mouse_exited() -> void:
 	$".".set_text("> Subject 1: Calihan Lonan <")
 
 func _on_pressed() -> void:
-	pass
+	get_tree().change_scene_to_file("res://Scenes/Levels/DreamCutscene1.tscn")
