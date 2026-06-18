43afec22a77897b1f883766154e59c3d3dbe4e6e
Atticus <callum01eales@gmail.com>
2026-06-18 18:03:35 +0100

    leaned on any railings lately?

--- Scripts/ReturnToMenuButtonScript.gd
+++ Scripts/ReturnToMenuButtonScript.gd
@@ -7,4 +7,5 @@ func _on_mouse_exited() -> void:
 	$".".set_text("> Return to Menu <")
 
 func _on_pressed() -> void:
+	Globals._restart()
 	get_tree().change_scene_to_file("res://Scenes/User Interface/Menus/MainMenu.tscn")
