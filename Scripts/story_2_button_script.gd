bac18e0638a8b8fe7b52ed9df7fa4e98afe53f08
Atticus <callum01eales@gmail.com>
2026-08-28 17:12:41 +0100

    IT IS FOOKEN MINGING

--- Scripts/story_2_button_script.gd
+++ Scripts/story_2_button_script.gd
@@ -1,10 +1,10 @@
 extends Button
 
 func _on_mouse_entered() -> void:
-	$".".set_text(">> Subject 2 - Temperance <<")
+	$".".set_text(">> Subject 2 - The Tower <<")
 
 func _on_mouse_exited() -> void:
-	$".".set_text("> Subject 2 - Temperance <")
+	$".".set_text("> Subject 2 - The Tower <")
 
 func _on_pressed() -> void:
 	$"../../AmyPanel/AnimationPlayer".play("Show")
